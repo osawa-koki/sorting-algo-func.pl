e4d8b683cbfdef83533a50ad8401ea15597f490f
osawa-koki <github@osawa.dev>
2023-02-20 13:37:32 +0900

    シャッフル関数定義した。

--- app/main.pl
+++ app/main.pl
@@ -1,6 +1,8 @@
 
-require "./app/display.pl";
+require "display.pl";
+require "shuffle.pl";
 
 my @array = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-display("Original", @array);
+Display("Original", @array);
+Display("Shuffled", Shuffle(@array));
