e82d41c749e2465d3c53c097387b4a651cf1402a
osawa-koki <github@osawa.dev>
2023-02-20 13:44:20 +0900

    マージソート。

--- app/main.pl
+++ app/main.pl
@@ -4,6 +4,7 @@ require "shuffle.pl";
 require "bubble_sort.pl";
 require "selection_sort.pl";
 require "insertion_sort.pl";
+require "merge_sort.pl";
 
 my @array = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
@@ -17,3 +18,6 @@ Display("Selection Sort", SelectionSort(@array));
 
 Display("Shuffled", Shuffle(@array));
 Display("Insertion Sort", InsertionSort(@array));
+
+Display("Shuffled", Shuffle(@array));
+Display("Merge Sort", MergeSort(@array));
